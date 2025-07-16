b9f55746f029e6978f151fac4817264496987e3a
Temirbek <temirbek@gmail.com>
2025-07-16 12:05:57 +0600

    sitemap, rss

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from django.db.models import Count
+import markdown
+from django.utils.safestring import mark_safe
 from ..models import Post
 
 register = template.Library()
@@ -18,3 +20,7 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(
         total_comments = Count('comments')
     ).order_by('-total_comments')[:count]
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
